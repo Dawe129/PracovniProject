cc39dc7870e7cbcfb7a8ba26c22586d7b9085e7a
Dawe129 <161817982+Dawe129@users.noreply.github.com>
2024-05-28 14:29:13 +0200

    dalsi ulozeni ale uz to vypada lip

--- src/CteniCSV.java
+++ src/CteniCSV.java
@@ -1,40 +1,20 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class CteniCSV {
-    public static Set<String> nactiCSV(String cesta) {
-        Set<String> data = new HashSet<>();
-        String radek;
-        String oddelovac = ",";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(cesta))) {
-            while ((radek = br.readLine()) != null) {
-                String[] hodnoty = radek.split(oddelovac);
-                data.add(hodnoty[0]);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return data;
-    }
-    public static Set<String> nactiBurdaObjednavky(String nazevSouboru) {
-        Set<String> objednavky = new HashSet<>();
+    public static void vypisBurdaObjednavky(String nazevSouboru) {
         try (BufferedReader br = new BufferedReader(new FileReader(nazevSouboru))) {
             String radek;
             while ((radek = br.readLine()) != null) {
-                String[] sloupce = radek.split(",");
-                if (sloupce.length > 14 && sloupce[14].startsWith("Burda")) { // předpokládáme, že druhý sloupec je značka
-                    objednavky.add(sloupce[0]); // předpokládáme, že první sloupec je ID objednávky
+                String[] sloupce = radek.split(";");
+                if (sloupce.length > 14 && sloupce[14].startsWith("\"Burda")) {
+                    System.out.println(radek);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return objednavky;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
-import java.util.List;
-import java.util.Set;
-
 public class Main {
-    public static void main(String[] args) {
 
-        String nazevSouboru = "ExportObjednavek.csv"; // Zňte na skutečný název souboru
-        Set<String> burdaObjednavky = CteniCSV.nactiBurdaObjednavky(nazevSouboru);
-        System.out.println("Burda objednávky:");
-        for (String objednavka : burdaObjednavky) {
-            System.out.println(objednavka);
-        }
+    public static void main(String[] args) {
+        String nazevSouboru = "ExportObjednavek.csv"; // Změňte na skutečný název souboru
+        CteniCSV.vypisBurdaObjednavky(nazevSouboru);
     }
 }
