9b846eb593c20f880f632f2fd98691e83cdf5e74
Dawe129 <161817982+Dawe129@users.noreply.github.com>
2024-05-28 14:03:18 +0200

    pridani predelaneho souboru + cteni obou souboru

--- src/CteniCSV.java
+++ src/CteniCSV.java
@@ -2,23 +2,39 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CteniCSV {
-
-    public static List<String[]> CSV(String cesta) {
-        List<String[]> data = new ArrayList<>();
-        String oddelovac = ",";
+    public static Set<String> nactiCSV(String cesta) {
+        Set<String> data = new HashSet<>();
         String radek;
+        String oddelovac = ",";
 
-        try (BufferedReader br = new BufferedReader(new FileReader("burdastyle_send_export_objednavek.csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(cesta))) {
             while ((radek = br.readLine()) != null) {
                 String[] hodnoty = radek.split(oddelovac);
-                data.add(hodnoty);
+                data.add(hodnoty[13]);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return data;
+    }
+    public static Set<String> nactiBurdaObjednavky(String nazevSouboru) {
+        Set<String> objednavky = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(nazevSouboru))) {
+            String radek;
+            while ((radek = br.readLine()) != null) {
+                String[] sloupce = radek.split(",");
+                if (sloupce[13].startsWith("Burda")) { // předpokládáme, že druhý sloupec je značka
+                    objednavky.add(sloupce[0]); // předpokládáme, že první sloupec je ID objednávky
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return objednavky;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
-        CteniCSV csv = new CteniCSV();
-        System.out.println(CteniCSV.CSV("C:\\Users\\Honza\\IdeaProjects\\PracovniProject\\burdastyle_send_export_objednavek.csv"));
+        String cesta = "C:\\Users\\dpivo\\Documents\\GitHub\\Zadani_2\\geo_coded_DM_jaro_2024.csv";
+        Set<String> data = CteniCSV.nactiCSV(cesta);
 
+        for (String radek : data) {
+            for (String hodnota : radek) {
+                System.out.print(hodnota + " ");
+            }
+            System.out.println();
+        }
     }
 }
